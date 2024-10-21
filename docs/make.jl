7be4cd71031d9fb814c3a061dce6ecebcffd9373
Orso Meneghini <orso82@gmail.com>
2024-10-21 12:00:48 -0700

    Add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[NEO],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="NEO",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
